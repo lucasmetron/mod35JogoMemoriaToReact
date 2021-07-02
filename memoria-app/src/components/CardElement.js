0e0ea7c69a4decc74a7f32299cc621c4aaf93bdc
lucasmetron <lucasmetron@gmail.com>
2021-07-02 08:43:50 -0300

    ajuste nas cartas

---
+++ memoria-app/src/components/CardElement.js
@@ -0,0 +1,20 @@
+import React from 'react';
+
+function CardElement(props) {
+
+
+
+    return (
+        <div id={props.card.id} className="card">
+            <div className="card_front">
+                <img className="icon" src={`assets/images/${props.card.icon}.png`} alt={props.card.icon} />
+            </div>
+
+            <div className="card_back">
+                {"</>"}
+            </div>
+        </div>
+    );
+};
+
+export default CardElement;
